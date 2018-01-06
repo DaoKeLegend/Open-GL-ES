7c0b1334cc6ec6c193893d29e17fa51ca93febea
DaokeLegend <lijunyuwork@163.com>
2018-01-06 21:53:27 +0800

    绘图
    三角形

---
+++ Open GL ES_demo1/Open GL ES_demo1/JJOpenglesVC.h
@@ -0,0 +1,14 @@
+//
+//  openglesVC.h
+//  Open GL ES_demo1
+//
+//  Created by lucy on 2018/1/6.
+//  Copyright © 2018年 com.daoKeLegend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface JJOpenglesVC : GLKViewController
+
+@end
